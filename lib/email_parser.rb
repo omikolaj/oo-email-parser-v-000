977dacb89190522c7eff716644037eb028b366d1
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-13 10:52:56 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,25 +3,17 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser
-
   attr_accessor :emails
-
-
 
   def initialize(emails)
     @emails= emails
   end
 
   def parse
-    temp = []
-    emails.split(" ").each do |w|
-        if(w.include?(","))
-          temp << w.gsub(",","")
-        else
-          temp << w
-        end
-    end
-    unique(temp)
+    binding.pry
+    emails.split.map do |email|
+      email.split(',')
+    end.flatten.uniq
   end
 
   def unique(array)
@@ -33,6 +25,4 @@ class EmailParser
     end
     unique
   end
-
-
 end
